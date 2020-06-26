15b07f2729f09f6ab8d6cbef2850e2b4feaac6d1
Fidanrlee <fidan.nonnnn5@gmail.com>
2020-06-26 10:40:39 +0400

    Python files added

---
+++ Python/Time_Delta.py
@@ -0,0 +1,28 @@
+
+import os
+from datetime import datetime
+
+# Complete the time_delta function below.
+def time_delta(t1, t2):
+    frmt = "%a %d %b %Y %H:%M:%S %z"
+    t1 = datetime.strptime(t1,frmt)
+    t2 = datetime.strptime(t2,frmt)
+    result = abs(t1-t2).total_seconds()
+    return str(int(result))
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        t1 = input()
+
+        t2 = input()
+
+        delta = time_delta(t1, t2)
+
+        fptr.write(delta + '\n')
+
+    fptr.close()
